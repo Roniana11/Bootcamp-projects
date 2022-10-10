eb403cc3514d4feb1c5e2bef91f76e9cc364256e
roniana11 <roniana11@gmail.com>
2022-10-10 12:04:16 +0300

    restoring dreamteammodel files

---
+++ NBA/client/models/dreamTeamModel.js
@@ -0,0 +1,16 @@
+"use strict";
+class DreamTeam {
+    constructor() {
+        this.dreamTeam = {};
+    }
+    getDreamTeam() {
+        return this.dreamTeam;
+    }
+    cleanDreamTeam() {
+        this.dreamTeam = {};
+    }
+    addToDreamTeam(playerID) {
+    }
+    removeFromDreamTeam(playerID) {
+    }
+}
---
+++ NBA/client/models/dreamTeamModel.ts
@@ -0,0 +1,22 @@
+class DreamTeam {
+    private dreamTeam: object = {};
+  
+    getDreamTeam() {
+      return this.dreamTeam;
+    }
+  
+    cleanDreamTeam(){
+      this.dreamTeam = {};
+    }
+  
+    addToDreamTeam(playerID:string){
+  
+    }
+
+    removeFromDreamTeam(playerID:string){
+  
+    }
+  
+   
+  }
+  
