9dfc14a30594d4cfade8024b1de5d6cf975ff305
roniana11 <roniana11@gmail.com>
2022-10-10 12:05:08 +0300

    add error handeling in server side

--- NBA/server/server.py
+++ NBA/server/server.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,HTTPException
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes import dream_team,stats
 from services.nba_api import get_players 
@@ -7,15 +8,27 @@ from services.nba_api import get_players
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(dream_team.router)
 app.include_router(stats.router)
 
 @app.get('/')
 async def get_team(year: int, team: str):
-    players = await get_players(year,team)
-    if not players:
-        raise HTTPException(status_code=404, detail="could not find players")
-    return players
+    try:
+        players = await get_players(year,team)
+        return players
+    except KeyError as e:
+        raise HTTPException(status_code=400, detail= e.args[0])
+    except Exception as e:
+        raise HTTPException(status_code=404, detail=e.args[0])
+    except:
+        raise HTTPException(status_code=500, detail="something went wrong")
 
 if __name__ =="__main__":
       uvicorn.run(app, host="0.0.0.0", port=8000)
--- NBA/server/services/nba_api.py
+++ NBA/server/services/nba_api.py
@@ -1,28 +1,31 @@
+from msilib.schema import Error
 import requests
 
 async def get_players(year,team):
+    # try:
     teams = await fetch_data(year,'teams')
     players = await fetch_data(year,'players')
+    team_id = get_team_id(teams,team)
+    team_players = [player for player in players if player["teamId"] == team_id]
+    
+    return team_players
+    # except KeyError:
 
-    if not (players or teams):
-        return {"Message": "Object not found."}
-    else:
-        team_id = get_team_id(teams,team)
-        team_players = [player for player in players if player["teamId"] == team_id]
-        
-        return team_players
+    # except Exception as e:
+    #     print(e["message"])
+    #     raise e
+
 
 async def fetch_data(year,data):
-    try:
         res = requests.get(url= f'https://data.nba.net/10s/prod/v1/{year}/{data}.json')
+        if res.status_code == 404:
+            raise Exception("There is no information for that specific year")
         return res.json()["league"]["standard"]
-    except Exception as e:
-        #check what kind of error and return the message
-        print(e)
+
 
 def get_team_id(teams,team_name):
-    id = next((team["teamId"] for team in teams if team["teamShortName"] == team_name), None)
+    id = next((team["teamId"] for team in teams if team["urlName"] == team_name), None)
     if id is None:
-        raise NameError("team does not exists")
+        raise KeyError("Team does not exists")
     
     return id
