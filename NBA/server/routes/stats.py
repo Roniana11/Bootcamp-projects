fc2730160023deaf21c155209b3050ca275d2668
roniana11 <roniana11@gmail.com>
2022-10-15 15:58:55 +0300

    adding route

--- NBA/server/routes/stats.py
+++ NBA/server/routes/stats.py
@@ -1,8 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
+from services.nba_api import fetch_stats
 
-router = APIRouter(prefix="/stats")
+router = APIRouter()
 
-@router.get('/{name}')
-async def get_stats():
-    # should return stats about the player
-    return
+@router.get('/',status_code=200)
+async def get_stats(last_name: str, first_name: str):
+    try:
+        stats = await fetch_stats(last_name,first_name)
+        return {"stats": stats}
+    except Exception as e:
+        raise HTTPException(status_code=404, detail= e.args[0])
+    except:
+        raise HTTPException(status_code=500, detail="something went wrong")
