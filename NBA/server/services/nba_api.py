10e3375fddd60347575cc065d4cbe0ee2cd79b40
roniana11 <roniana11@gmail.com>
2022-10-15 15:58:27 +0300

    adding fetch stats

--- NBA/server/services/nba_api.py
+++ NBA/server/services/nba_api.py
@@ -1,20 +1,13 @@
-from msilib.schema import Error
+import json
 import requests
 
 async def get_players(year,team):
-    # try:
     teams = await fetch_data(year,'teams')
     players = await fetch_data(year,'players')
     team_id = get_team_id(teams,team)
     team_players = [player for player in players if player["teamId"] == team_id]
     
     return team_players
-    # except KeyError:
-
-    # except Exception as e:
-    #     print(e["message"])
-    #     raise e
-
 
 async def fetch_data(year,data):
         res = requests.get(url= f'https://data.nba.net/10s/prod/v1/{year}/{data}.json')
@@ -29,3 +22,12 @@ def get_team_id(teams,team_name):
         raise KeyError("Team does not exists")
     
     return id
+
+async def fetch_stats(last_name,first_name):
+        res = requests.get(url= f'https://nba-players.herokuapp.com/players-stats/{last_name}/{first_name}')
+        try:
+            data = json.loads(res.text)
+            return data
+        except:
+            raise Exception("There is no information for that specific player")
+        
