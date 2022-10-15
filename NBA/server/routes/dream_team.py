c48f6d5187bc7643a6f0c6b024d527d976a37590
roniana11 <roniana11@gmail.com>
2022-10-15 15:59:39 +0300

    wrting get post and delete routes

--- NBA/server/routes/dream_team.py
+++ NBA/server/routes/dream_team.py
@@ -1,24 +1,34 @@
+from fastapi import Request,HTTPException
 from fastapi import APIRouter
+import json
 
-router = APIRouter(prefix="/dreamteam")
+router = APIRouter()
 
-@router.get('/')
-async def get_dream_team():
-    return
+from models.dream_team_model import DreamTeam 
+team = DreamTeam()
 
-@router.post('/{name}')
-async def add_players():
-    return
+@router.get('/',status_code=200)
+def get_dream_team():
+    team_dict = team.getDreamTeam()
+    return json.dumps(list(team_dict.values()))
 
-@router.delete('/{name}')
-async def delete_players():
-    return
+@router.post('/', status_code=201)
+async def add_player(request: Request):
+    try:
+        player_data = await request.json()
+        team.addPlayer(player_data)
+        return {"player": player_data}
+    except KeyError as e:
+        raise HTTPException(status_code=400, detail= e.args[0])
+    except:
+        raise HTTPException(status_code=500, detail="something went wrong")
 
-#========= an option for multiple operation==============
-# @router.post('/')
-# async def add_players():
-#     return
 
-# @router.delete('/')
-# async def delete_players():
-#     return
+@router.delete('/{id}',status_code=204)
+def delete_player(id):
+    try:
+        team.removePlayer(id)
+    except KeyError as e:
+        raise HTTPException(status_code=404, detail= e.args[0])
+    except:
+        raise HTTPException(status_code=500, detail="something went wrong")
