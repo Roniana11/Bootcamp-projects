f59b929876a3c5491b0d98cc04e9e82a8cc56b63
roniana11 <roniana11@gmail.com>
2022-10-15 16:03:40 +0300

    fixing load method

--- NBA/client/models/playersModel.js
+++ NBA/client/models/playersModel.js
@@ -15,6 +15,10 @@ class Players {
     getPlayers() {
         return this.players;
     }
+    getPlayerData(id) {
+        const player_data = this.players.find(p => p.id === id) || { name: ' ' };
+        return player_data;
+    }
     cleanPlayers() {
         this.players.length = 0;
     }
@@ -22,15 +26,15 @@ class Players {
     loadPlayers(teamName, year) {
         return __awaiter(this, void 0, void 0, function* () {
             const data = yield fetchPlayers(teamName, year);
+            this.cleanPlayers();
             this.players = data.map((player) => {
                 //find a solution for any here
                 return {
-                    id: player.id,
-                    name: player.firstName + " " + player.lastName,
+                    id: player.personId,
+                    name: player.firstName.toUpperCase() + " " + player.lastName.toUpperCase(),
                     jersey: player.jersey,
                     position: player.pos,
                     image: `https://nba-players.herokuapp.com/players/${player.lastName}/${player.firstName}`,
-                    isInDreamTeam: false,
                 };
             });
         });
--- NBA/client/models/playersModel.ts
+++ NBA/client/models/playersModel.ts
@@ -5,6 +5,11 @@ class Players {
     return this.players;
   }
 
+  getPlayerData(id: string){
+    const player_data = this.players.find(p => p.id === id) || {name:' '};
+    return player_data;
+  }
+
   cleanPlayers() {
     this.players.length = 0;
   }
@@ -13,15 +18,15 @@ class Players {
 
   async loadPlayers(teamName: string, year: number) {
     const data = await fetchPlayers(teamName, year);
+    this.cleanPlayers()
     this.players = data.map((player: any) => {
       //find a solution for any here
       return {
-        id: player.id,
-        name: player.firstName + " " + player.lastName,
+        id: player.personId,
+        name: player.firstName.toUpperCase() + " " + player.lastName.toUpperCase(),
         jersey: player.jersey,
         position: player.pos,
         image: `https://nba-players.herokuapp.com/players/${player.lastName}/${player.firstName}`,
-        isInDreamTeam: false,
       };
     });
   }
